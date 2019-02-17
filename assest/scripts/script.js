92ff32e1c520f78bd1b4a72993aa5804ee6a42a8
Panakj <pankaj.csdeveloper@gmail.com>
2019-02-17 21:47:13 +0530

    css class added

---
+++ assest/scripts/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $('.select-opt').on('click', function(){
+        $(this).parent().toggleClass('active').siblings().removeClass('active');
+    })
+    $('.form-row').find('li').on('click', function(){
+        $(this).parent().siblings('.select-opt').val( $(this).text() );
+        $(this).addClass('active').siblings().removeClass('active');
+    })
+})
